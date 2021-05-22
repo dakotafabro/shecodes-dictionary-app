98c9d41002378148e3992267a0d55b53d3c3705b
dakotafabro <dakotafabrocgu@gmail.com>
2021-05-22 13:59:43 -0700

    Added axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-// import headerImage from "./headerImage.png";
 import DictionarySearch from "./DictionarySearch";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        {/* <img className="header-image img-fluid m-2" src={headerImage} /> */}
         <DictionarySearch />
       </div>
     </div>
--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
-  let [keyword, setKeyword] = useState(" ");
+  let [keyword, setKeyword] = useState("");
+  let [keywordData, setKeywordData] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
   function search(event) {
     event.preventDefault();
-    console.log(keyword);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en-US/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyword(event) {
@@ -14,15 +23,21 @@ export default function DictionarySearch() {
   }
 
   return (
-    <div className="DictionarySearch">
-      <form className="m-3" onSubmit={search}>
-        <input
-          className="search-field"
-          type="search"
-          onChange={handleKeyword}
-          placeholder="Search for a word"
-        />
-        <input className="submit-button" type="submit" value="Define" />
+    <div className="DictionarySearch mt-3 shadow">
+      <form className="m-5" onSubmit={search}>
+        <div className="row">
+          <input
+            className="search-field col-sm-10"
+            type="search"
+            onChange={handleKeyword}
+            placeholder="Search for a word"
+          />
+          <input
+            className="submit-button col-sm-2"
+            type="submit"
+            value="Define"
+          />
+        </div>
       </form>
     </div>
   );
