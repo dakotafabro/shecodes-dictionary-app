e5c7c2f51b27bbf8b7d20e94732851705b4f8cb6
dakotafabro <dakotafabrocgu@gmail.com>
2021-05-23 15:33:57 -0700

    Update Images.js

--- src/Images.js
+++ src/Images.js
@@ -4,22 +4,23 @@ import "./Images.css";
 export default function Images(props) {
   if (props.photos) {
     return (
-    <div className="Images">
-      <div className="row">
-        {props.photos.map(function (photo, index) {
-          return (
-            <div className="col-sm-4" key={index}>
-              <img
-                src={photo.src.tiny}
-                className="keyword-photos img-fluid m-3"
-                alt={photo.src.tiny}
-              />
-            </div>
-          );
-        })}
+      <div className="Images">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-sm-4" key={index}>
+                <img
+                  src={photo.src.tiny}
+                  className="keyword-photos img-fluid m-3"
+                  alt={photo.src.tiny}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
-} else {
+    );
+  } else {
     return null;
+  }
 }
